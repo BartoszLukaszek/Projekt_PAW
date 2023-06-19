58b13cb71eb7b5d6f4c44f62cdf7ef69d575e564
BartoszLukaszek <barti120@onet.pl>
2023-06-19 18:33:16 +0200

    Add (new Project, Edit, Delete, Update, sccs)

--- Projekt/src/app/app.module.ts
+++ Projekt/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectlistComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProjectListComponent
-  ],
+    ProjectlistComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    RouterModule,
+    AppRoutingModule,
+    FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Projekt/src/app/project-list/project-list.component.ts
+++ Projekt/src/app/project-list/project-list.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
-  selector: 'app-project-list',
+  selector: 'app-projectlist',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.scss']
+  styleUrls: ['./project-list.component.scss'],
 })
-export class ProjectListComponent {
 
+export class ProjectlistComponent implements OnInit {
+  posts: Post[] = [];
+  newPost: Post = { title: '', content: '' };
+  editedPost: Post | null = null;
+  editedPostIndex: number = -1;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+  }
+
+
+  addPost(): void {
+    this.postService.addPost(this.newPost);
+    this.newPost = { title: '', content: '' };
+    this.posts = this.postService.getPosts();
+  }
+
+  deletePost(index: number): void {
+    this.postService.deletePost(index);
+    this.posts = this.postService.getPosts();
+  }
+
+  enterEditMode(post: Post, index: number): void {
+    this.editedPost = { ...post };
+    this.editedPostIndex = index;
+  }
+
+  cancelEdit(): void {
+    this.editedPost = null;
+    this.editedPostIndex = -1;
+  }
+
+  updatePost(): void {
+    if (this.editedPost && this.editedPostIndex !== -1) {
+      this.postService.editPost(this.editedPostIndex, this.editedPost);
+      this.cancelEdit();
+      this.posts = this.postService.getPosts();
+    }
+  }
 }
