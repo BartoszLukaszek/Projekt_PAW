8a0334bc85e0bf9e596c3ec6d262e98d2bd40297
BartoszLukaszek <barti120@onet.pl>
2023-06-23 02:14:25 +0200

    Add project-details

---
+++ Projekt/src/app/project-details/project-details.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../services/post.service';
+
+interface Post {
+  id?: number;
+  title: string;
+  content: string;
+  status: 'Do zrobienia' | 'W trakcie' | 'Zrobione';
+  funkcjonalnosc: string;
+  priority: 'Wysoki' | 'Średni' | 'Niski';
+  details?: string;
+}
+
+@Component({
+  selector: 'app-project-details',
+  templateUrl: './project-details.component.html',
+  styleUrls: ['./project-details.component.scss'],
+})
+export class ProjectDetailsComponent implements OnInit {
+  @Input() post!: Post;
+  @Output() update = new EventEmitter<Post>();
+  functionalities: string[] = [];
+  isEditMode = false;
+  editedPost: Post = {
+    title: '',
+    content: '',
+    status: 'Do zrobienia',
+    funkcjonalnosc: '',
+    priority: 'Niski',
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      const post = this.postService.getPost(Number(id));
+      if (post !== null) {
+        this.post = post;
+        this.editedPost = { ...post };
+      }
+    }
+    this.loadFunctionalities();
+  }
+
+  loadFunctionalities(): void {
+    this.functionalities = ['Functionality 1', 'Functionality 2', 'Functionality 3'];
+  }
+
+  onEdit(): void {
+    this.isEditMode = true;
+  }
+
+  onCancel(): void {
+    this.isEditMode = false;
+    this.editedPost = { ...this.post };
+  }
+
+  onUpdate(): void {
+    this.update.emit(this.editedPost);
+    this.post = { ...this.editedPost };
+    this.isEditMode = false;
+  }
+}
